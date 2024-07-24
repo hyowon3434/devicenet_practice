857f124a3e7f0fb95e4b7066d671083aeaa35585
hyowon3434 <hyowon3434@naver.com>
2024-07-24 23:46:49 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+
+
+
+app.get('/', (req, res)=> {
+    res.send('Hello, Node!~')
+}) 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use('/contacts', require('./routes/contactRoutes'))
+
+app.listen(3000, () => {
+    console.log('Server is Running')
+})
